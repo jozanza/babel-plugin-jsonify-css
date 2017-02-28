4fdc3c9483930add0eb99e947d5a1d6bb578d31e
jozanza <hello@jsavary.com>
2017-02-28 15:33:19 -0500

    added files

---
+++ README.md
@@ -0,0 +1,27 @@
+# babel-plugin-jsonify-css
+
+[![NPM version][npm-image]][npm-url] [![Downloads][downloads-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency status][david-dm-image]][david-dm-url] [![Dev Dependency status][david-dm-dev-image]][david-dm-dev-url] [![Coverage Status][coveralls-image]][coveralls-url]
+
+[npm-url]:https://npmjs.org/package/babel-plugin-jsonify-css
+[downloads-image]:http://img.shields.io/npm/dm/babel-plugin-jsonify-css.svg
+[npm-image]:http://img.shields.io/npm/v/babel-plugin-jsonify-css.svg
+[travis-url]:https://travis-ci.org/jozanza/babel-plugin-jsonify-css
+[travis-image]:http://img.shields.io/travis/jozanza/babel-plugin-jsonify-css/master.svg
+[david-dm-url]:https://david-dm.org/jozanza/babel-plugin-jsonify-css
+[david-dm-image]:https://img.shields.io/david/jozanza/babel-plugin-jsonify-css.svg
+[david-dm-dev-url]:https://david-dm.org/jozanza/babel-plugin-jsonify-css#info=devDependencies
+[david-dm-dev-image]:https://img.shields.io/david/dev/jozanza/babel-plugin-jsonify-css.svg
+[coveralls-image]:https://coveralls.io/repos/github/jozanza/babel-plugin-jsonify-css/badge.svg?branch=master
+[coveralls-url]:https://coveralls.io/github/jozanza/babel-plugin-jsonify-css?branch=master
+
+## Installation
+
+`$ yarn add --dev babel-plugin-jsonify-css`
+
+## Usage
+
+TODO
+
+## License
+
+[MIT License](http://opensource.org/licenses/MIT)
---
+++ index.js
@@ -0,0 +1,67 @@
+const fs = require('fs');
+const p = require('path');
+const jsonify = require('jsonify-css');
+
+const exit = t => (path, state) => {
+  if (!isImportingCSS(path)) return;
+  const decls = getImportDecls(path);
+  const filepath = getFilepath(path, state);
+  const css = getCSS(filepath);
+  const styles = jsonify({ root: p.dirname(filepath) })(css)
+  const objify = obj =>
+    t.objectExpression(Object.keys(obj)
+      .map(k => t.objectProperty(
+        t.stringLiteral(k),
+        'object' === typeof obj[k]
+          ? objify(obj[k])
+          : t.stringLiteral(obj[k])
+      )))
+  const memberImports = decls.member.map(x =>
+    t.variableDeclaration('var', [
+      t.variableDeclarator(
+        t.identifier(x.imported.name),
+        t.arrayExpression(styles[x.imported.name].map(objify))
+      )
+    ]))
+  const fullImports = decls.full.map(x =>
+    t.variableDeclaration('var', [
+      t.variableDeclarator(
+        t.identifier(x.local.name),
+          objify(
+            styles.rule.concat(styles.media)
+            .reduce((a, b) => Object.assign({}, a, b), {})
+          ))
+    ]))
+  path.replaceWithMultiple([
+    ...memberImports,
+    ...fullImports,
+  ])
+}
+
+const isImportingCSS = path =>
+  '.css' === p.parse(path.node.source.value).ext;
+
+const getImportDecls = path => ({
+  full: path.node.specifiers
+    .filter(x => x.type !== 'ImportSpecifier'),
+  member: path.node.specifiers
+    .filter(x => x.type === 'ImportSpecifier'),
+});
+
+const getFilepath = (path, state) => {
+  const dir = state.file.opts.filename[0] === '.'
+    ? p.dirname(p.resolve(state.file.opts.filename))
+    : 'node_modules';
+  return p.resolve(dir, path.node.source.value);
+};
+
+const getCSS = filepath =>
+  fs.readFileSync(filepath, 'utf8');
+
+module.exports = opts => ({
+  visitor: {
+    ImportDeclaration: {
+      exit: exit(opts.types)
+    }
+  }
+});
